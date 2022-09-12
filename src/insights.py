6050a9b8f1c1bfa7dacdb42b6a3b28e683def0f9
R-R-Freitas <rodrigoaz8@gmail.com>
2022-09-12 18:48:33 -0300

    feat: get unique industries

--- src/insights.py
+++ src/insights.py
@@ -52,7 +52,11 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    return list(set([
+        job["industry"]
+        for job in read(path) if len(job["industry"]) > 0
+    ]))
+    # list(set(map(lambda job: job["industry"], read(path))))
 
 
 def filter_by_industry(jobs, industry):
