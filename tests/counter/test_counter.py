30b55ba86a2af87b3957de0bba65a6046b3e3854
R-R-Freitas <rodrigoaz8@gmail.com>
2022-09-12 21:46:37 -0300

    test: counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,14 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
+# import pytest
+path = "src/jobs.csv"
+words = ["Python", "Javascript", "Parangaricutirimicuaro"]
+results = [1639, 122, 0]
 
 
 def test_counter():
-    pass
+    for index in range(len(words)):
+        result = count_ocurrences(path, words[index])
+        assert result == results[index]
+
+
+test_counter()
