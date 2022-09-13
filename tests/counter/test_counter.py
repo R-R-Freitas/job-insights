535ab2c3b3617b44d388499ba7dc5491cdb5db43
R-R-Freitas <rodrigoaz8@gmail.com>
2022-09-12 23:09:48 -0300

    test: refactor: improving counter tests

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -3,12 +3,18 @@ from src.counter import count_ocurrences
 path = "src/jobs.csv"
 words = ["Python", "Javascript", "Parangaricutirimicuaro"]
 results = [1639, 122, 0]
+other_path = "tests/mocks/jobs.csv"
+more_words = ["Full", "Developer", "End"]
+more_results = [3, 3, 3]
 
 
 def test_counter():
     for index in range(len(words)):
         result = count_ocurrences(path, words[index])
         assert result == results[index]
+    for index in range(len(more_words)):
+        result = count_ocurrences(other_path, more_words[index])
+        assert result == more_results[index]
 
 
 test_counter()
