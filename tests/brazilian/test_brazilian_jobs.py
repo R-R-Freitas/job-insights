2312642a8e80610a96590468172bfeca101b76c2
R-R-Freitas <rodrigoaz8@gmail.com>
2022-09-12 22:08:34 -0300

    test: read brazilian jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+
+
+path = "tests/mocks/brazilians_jobs.csv"
+keys = ["title", "salary", "type"]
 
 
 def test_brazilian_jobs():
-    pass
+    jobs = read_brazilian_file(path)
+    for job in jobs:
+        for key in keys:
+            assert key in job.keys()
